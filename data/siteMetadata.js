5096c1df06fc6afb9edcb6c9586414e9cb179e95
Rabby Hasan <rabbyalone@gmail.com>
2023-08-31 20:04:51 +0600

    updated default data

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,9 +1,9 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: '.NET blog from Rabby Hasan',
+  author: 'Mohammed Rabby Hasan',
+  headerTitle: 'R.Hasan Blog',
+  description: 'A blog by 7+ years of experienced software developer',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
