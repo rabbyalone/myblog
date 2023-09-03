78010ad93b5aac04bba399849eb36aeec1c00f26
Rabby Hasan <rabbyalone@gmail.com>
2023-09-03 08:54:50 +0600

    code cleanup and added porfolio button

--- app/Main.tsx
+++ app/Main.tsx
@@ -19,6 +19,13 @@ export default function Home({ posts }) {
             Insights from an experienced senior full stack engineer who loves to develop
             applications from passion.
           </p>
+          <a
+            type="button"
+            href="https://www.rabbyhasan.com.bd/"
+            className="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+          >
+            See my portfolio
+          </a>
         </div>
       </div>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
