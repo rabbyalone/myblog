f5455b3c8b61d7c2667ee3a3deee2475e57f1b6c
Rabby Hasan <rabbyalone@gmail.com>
2023-09-04 19:10:08 +0600

    added loader in react component

--- app/Main.tsx
+++ app/Main.tsx
@@ -3,12 +3,22 @@ import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 import { formatDate } from 'pliny/utils/formatDate'
 import NewsletterForm from 'pliny/ui/NewsletterForm'
+import { useState, useEffect } from 'react'
+import LoadingSpinner from '@/components/LoadingSpinner'
 
 const MAX_DISPLAY = 5
 
 export default function Home({ posts }) {
+  const [loading, setIsLoading] = useState(true)
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false) // Set isLoading to false after a delay
+    }, 50) // Simulate loading for 3 seconds
+  }, [])
   return (
     <>
+      {loading && <LoadingSpinner />}
+
       <div className="flex items-center justify-between mt-7">
         <div className="space-y-4">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
--- app/tags/[tag]/page.tsx
+++ app/tags/[tag]/page.tsx
@@ -9,6 +9,7 @@ import { genPageMetadata } from 'app/seo'
 import { Metadata } from 'next'
 import { useState, useEffect } from 'react'
 import apiService from 'utils/ApiService'
+import LoadingSpinner from '@/components/LoadingSpinner'
 
 // export async function generateMetadata({ params }: { params: { tag: string } }): Promise<Metadata> {
 //   const tag = params.tag
@@ -37,18 +38,27 @@ export default function TagPage({ params }: { params: { tag: string } }) {
   const { tag } = params
   // Capitalize first letter and convert space to dash
   const title = tag[0].toUpperCase() + tag.split(' ').join('-').slice(1)
+  const [loading, setLoading] = useState(false)
   const [posts, setPosts] = useState([])
 
   useEffect(() => {
     const fetchPosts = async () => {
       try {
+        setLoading(true)
         const response = await apiService.get(`/api/posts/postbytag?tag=${tag}`)
         setPosts(response.data)
       } catch (error) {
         console.error('Error fetching posts:', error)
+      } finally {
+        setLoading(false)
       }
     }
     fetchPosts()
   }, [])
-  return <ListLayout posts={posts} title={title} />
+  return (
+    <>
+      {loading && <LoadingSpinner />}
+      <ListLayout posts={posts} title={title} />
+    </>
+  )
 }
