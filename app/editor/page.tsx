56f342a17737c773fe91196f5d65797c6fd2b92a
Rabby Hasan <rabbyalone@gmail.com>
2024-01-04 00:54:10 +0600

    added isdraft and toaster

--- app/editor/page.tsx
+++ app/editor/page.tsx
@@ -1,16 +1,15 @@
 'use client'
 import { Authors, allAuthors } from 'contentlayer/generated'
 import { coreContent } from 'pliny/utils/contentlayer'
-import { genPageMetadata } from 'app/seo'
-import CreatePostLayout from '@/layouts/CreatePostLayout'
 import '@mdxeditor/editor/style.css'
 import dynamic from 'next/dynamic'
-import { useRef, useState } from 'react'
+import { useState } from 'react'
 import apiService, { setAuthToken } from 'utils/ApiService'
 import MDEditor from '@uiw/react-md-editor'
 import TagsInput from 'react-tagsinput'
 import 'react-tagsinput/react-tagsinput.css'
 import LoadingSpinner from '@/components/LoadingSpinner'
+import toast, { Toaster } from 'react-hot-toast'
 
 const MDXEditor = dynamic(() => import('@mdxeditor/editor').then((mod) => mod.MDXEditor), {
   ssr: false,
@@ -55,6 +54,7 @@ export default function Page() {
   const [title, setTitle] = useState('')
   const [blogId, setBlog] = useState('')
   const [IsSaved, setSaved] = useState(false)
+  const [IsDraft, setDraft] = useState(false)
   const [secret, setSecret] = useState('')
   const [summary, setSummary] = useState('')
   const [tags, setTags] = useState([])
@@ -70,14 +70,17 @@ export default function Page() {
         title: title,
         summary: summary,
         layout: 'PostSimple',
+        isDraft: IsDraft,
         tags: tags,
         author: 'Mohammed Rabby Hasan',
         content: JSON.stringify(value),
       }
       if (IsEdit) {
         await apiService.put(`/api/posts/${blogId}`, post)
+        toast.success('UPDATED')
       } else {
         await apiService.post('/api/posts', post)
+        toast.success('Success')
       }
       setSaved(true)
       setIsLoading(false)
@@ -100,6 +103,7 @@ export default function Page() {
       setValue(parsedContent)
       setTitle(response.data.title)
       setSummary(response.data.summary)
+      setDraft(response.data.isDraft)
       setTags(response.data.tags)
       setIsEdit(true)
       setIsLoading(false)
@@ -123,6 +127,10 @@ export default function Page() {
     setTags(tags)
   }
 
+  const handleDraftChange = () => {
+    setDraft(!IsDraft)
+  }
+
   const handleEditorChange = (value, event, state) => {
     // Do something with the updated 'value' state
     setValue(value)
@@ -131,80 +139,89 @@ export default function Page() {
   return (
     <>
       {loading && <LoadingSpinner />}
-      <CreatePostLayout content={mainContent}>
-        {/* <MDXLayoutRenderer code={author.body.code} /> */}
-      </CreatePostLayout>
-      <div className="container mb-4">
-        <div className="p-2">
-          <input
-            type="text"
-            value={blogId}
-            onChange={(e) => setBlog(e.target.value)}
-            placeholder="Blog Id for update"
-          />
-          <button
-            className="bg-primary-500 p-2 ml-3 rounded text-white disabled:opacity-50"
-            onClick={loadPost}
-            disabled={blogId === ''}
-          >
-            {' '}
-            Load Post{' '}
-          </button>
-        </div>
-        <div className="p-2">
-          <input
-            type="text"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            placeholder="Title goes here"
-          />
-        </div>
-        <div className="p-2">
-          <input
-            type="text"
-            value={summary}
-            onChange={(e) => setSummary(e.target.value)}
-            placeholder="Summary goes here"
-          />
-        </div>
-        <div>
+      <div className="">
+        <div className="mb-4">
           <div className="p-2">
-            <TagsInput value={tags} onChange={handleTagsChange} />
-          </div>
-        </div>
-      </div>
-      <h1>Post Body</h1>
-      <hr />
-      <div>
-        {/* <MDXEditor markdown={'# Hello World'} onChange={(markdown) => editorOnchange(markdown)} /> */}
-        <MDEditor height={500} value={value} onChange={handleEditorChange} />
-      </div>
-      <div className="mt-3">
-        {!IsAuthorized ? (
-          <div>
             <input
               type="text"
-              value={secret}
-              onChange={(e) => setSecret(e.target.value)}
-              placeholder="Secret goes here"
+              value={blogId}
+              onChange={(e) => setBlog(e.target.value)}
+              placeholder="Blog Id for update"
             />
             <button
-              className="ml-2 mr-2 bg-green-500 p-2 mt-1 rounded text-white"
-              onClick={authorize}
+              className="bg-primary-500 p-2 ml-3 rounded text-white disabled:opacity-50"
+              onClick={loadPost}
+              disabled={blogId === ''}
             >
               {' '}
-              Authorize{' '}
+              Load Post{' '}
             </button>
           </div>
-        ) : (
-          <p>Authorized</p>
-        )}
-        {IsAuthorized && (
-          <button className="bg-primary-500 p-2 mt-1 rounded text-white" onClick={save}>
-            {IsEdit ? 'Update Post' : 'Save Post'}
-          </button>
-        )}
-        {IsSaved && <p>Success!!</p>}
+          <div className="p-2">
+            <input
+              type="text"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              placeholder="Title goes here"
+            />
+            <input
+              type="checkbox"
+              className="ml-3 w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+              checked={IsDraft}
+              onChange={handleDraftChange}
+            />{' '}
+            Is Draft
+          </div>
+          <div className="p-2">
+            <input
+              type="text"
+              value={summary}
+              onChange={(e) => setSummary(e.target.value)}
+              placeholder="Summary goes here"
+            />
+          </div>
+          <div>
+            <div className="p-2">
+              <TagsInput value={tags} onChange={handleTagsChange} />
+            </div>
+          </div>
+        </div>
+        <h1>Post Body</h1>
+        <hr />
+        <div>
+          <MDEditor height={500} value={value} onChange={handleEditorChange} />
+        </div>
+        <div className="mt-3">
+          {!IsAuthorized ? (
+            <div>
+              <input
+                type="text"
+                value={secret}
+                onChange={(e) => setSecret(e.target.value)}
+                placeholder="Secret goes here"
+              />
+              <button
+                className="ml-2 mr-2 bg-green-500 p-2 mt-1 rounded text-white"
+                onClick={authorize}
+              >
+                {' '}
+                Authorize{' '}
+              </button>
+            </div>
+          ) : (
+            <p>Authorized</p>
+          )}
+          {IsAuthorized && (
+            <>
+              (
+              <button className="bg-primary-500 p-2 mt-1 rounded text-white" onClick={save}>
+                {IsEdit ? 'Update Post' : 'Save Post'}
+              </button>
+              <button className="bg-sky-500 p-2 mt-1 rounded text-white">See Drafts</button>)
+            </>
+          )}
+          {IsSaved && <Toaster position="bottom-right" reverseOrder={false}></Toaster>}
+        </div>
       </div>
     </>
   )
